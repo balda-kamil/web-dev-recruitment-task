e8838b9f62c8dde4e98855e74c17682036885715
Kamil Balda <baldakamil@gmail.com>
2023-04-12 22:41:21 +0200

    change url path to api

--- src/client/services/accordionPage/getAccordionPageData.ts
+++ src/client/services/accordionPage/getAccordionPageData.ts
@@ -1,5 +1,3 @@
-
-
 export const getAccordionPageData = async () => {
-    return await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/chatbot-details`);
+    return await fetch(`http://localhost:3000/api/chatbot-details`);
 };
